0de4751179ac2bc4732ed049025a615764c7e373
Youssef Wagih <youssefwagih212@gmail.com>
2026-07-04 23:57:07 +0300

    implement Validators

--- backend/AuctionPlatform.Application/AutoBids/Validators/CreateAutoBidValidator.cs
+++ backend/AuctionPlatform.Application/AutoBids/Validators/CreateAutoBidValidator.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using AuctionPlatform.Application.AutoBids.DTOs;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AuctionPlatform.Application.AutoBids.Validators
 {
-    public class CreateAutoBidValidator
+    public class CreateAutoBidValidator : AbstractValidator<CreateAutoBidRequest>
     {
+        public CreateAutoBidValidator()
+        {
+            RuleFor(x => x.MaxAmount)
+                .GreaterThan(0)
+                .WithMessage("Maximum bid amount must be greater than zero.");
+        }
     }
 }
--- backend/AuctionPlatform.Application/AutoBids/Validators/UpdateAutoBidValidator.cs
+++ backend/AuctionPlatform.Application/AutoBids/Validators/UpdateAutoBidValidator.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using AuctionPlatform.Application.AutoBids.DTOs;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AuctionPlatform.Application.AutoBids.Validators
 {
-    public class UpdateAutoBidValidator
+    public class UpdateAutoBidValidator : AbstractValidator<UpdateAutoBidRequest>
     {
+        public UpdateAutoBidValidator()
+        {
+            RuleFor(x => x.MaxAmount)
+                .GreaterThan(0)
+                .WithMessage("Maximum bid amount must be greater than zero.");
+        }
     }
 }
