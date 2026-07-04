40ab678db3a0fa0d7ea0ba99dcd0fbb7166f404e
Youssef Wagih <youssefwagih212@gmail.com>
2026-07-05 00:35:43 +0300

    add AutoBid service

--- backend/AuctionPlatform.Application/AutoBids/Interfaces/IAutoBidService.cs
+++ backend/AuctionPlatform.Application/AutoBids/Interfaces/IAutoBidService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuctionPlatform.Application.AutoBids.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,28 @@ namespace AuctionPlatform.Application.AutoBids.Interfaces
 {
     public interface IAutoBidService
     {
+        Task<AutoBidDto> GetByIdAsync(Guid id, CancellationToken ct = default);
+
+        Task<AutoBidDto> GetUserAutoBidAsync(
+            Guid auctionId,
+            Guid bidderId,
+            CancellationToken ct = default);
+
+        Task<AutoBidDto> CreateAsync(
+            Guid auctionId,
+            Guid bidderId,
+            CreateAutoBidRequest request,
+            CancellationToken ct = default);
+
+        Task<AutoBidDto> UpdateAsync(
+            Guid id,
+            Guid bidderId,
+            UpdateAutoBidRequest request,
+            CancellationToken ct = default);
+
+        Task DeleteAsync(
+            Guid id,
+            Guid bidderId,
+            CancellationToken ct = default);
     }
 }
