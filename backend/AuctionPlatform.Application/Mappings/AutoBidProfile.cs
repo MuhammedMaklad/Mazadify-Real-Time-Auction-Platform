10b0468876faf75133d9269fb9dea151f52f1dc5
Youssef Wagih <youssefwagih212@gmail.com>
2026-07-05 00:36:12 +0300

    add AutoBid mapper profile

--- backend/AuctionPlatform.Application/Mappings/AutoBidProfile.cs
+++ backend/AuctionPlatform.Application/Mappings/AutoBidProfile.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using AuctionPlatform.Application.AutoBids.DTOs;
+using AuctionPlatform.Domain.Entities;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AuctionPlatform.Application.Mappings
 {
-    public class AutoBidProfile
+    public class AutoBidProfile : Profile
     {
+        public AutoBidProfile()
+        {
+            CreateMap<AutoBid, AutoBidDto>();
+        }
     }
 }
